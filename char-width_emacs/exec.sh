36f50e6a4e2a02b5d35acd385ed6e93c3f7ee48c
ein <ein@hpnull.localdomain>
2014-08-03 14:56:07 +0900

    Change format of char-width_*.txt

--- char-width_emacs/exec.sh
+++ char-width_emacs/exec.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+mkdir -p output
 echo "settings/my-utf-8-eaw-fullwidth.elを適用したときのchar-widthの値と"
 echo "cygwinのminttyが定義するwcwidth()の値の違いを確認します。"
 echo ""
 echo "minttyのソースsrc/newlib/libc/string/wcwidth.cにある"
 echo "ambiguous[]の{..., ...},という配列の中身をコピーし、"
 echo "ambiguous_buffer.txtとして保存しました。"
-
-echo "ambiguous_buffer.txtを読み込み各ユニコード文字のコードポイントからchar-widthの値を計算します。計算結果はchar-width_emacs.txtに保存されます"
+echo ""
+echo "ambiguous_buffer.txtを読み込み各ユニコード文字のコードポイントからchar-widthの値を計算します。"
+echo "計算結果はoutput/char-width_emacs-default.txtに保存されます。"
+echo "init.elをロードした後の計算結果はoutput/char-width_emacs-after-init-loaded.txtに保存されます。"
 emacs -batch -l current-emacs-ambiguous-width.el
-echo "width-averageの行を取り除き、char-width-list_emacs.txtに保存します"
-grep -o " \(1\|2\)$" char-width_emacs.txt > char-width-list_emacs.txt
-echo "See: char-width-list_emacs.txt."
+echo "Rangeの行を取り除き、output/char-width-list_default-emacs.txtに保存します"
+grep -v "^Range" output/char-width_emacs-default.txt > output/char-width-list_default-emacs.txt
+echo "See: output/char-width-list_default-emacs.txt."
 
